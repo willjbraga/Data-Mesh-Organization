35da90634980c34aaeec972666d543a283476d7a
LeandroOliveiraVS <leandro.oliveiravs@gmail.com>
2026-05-19 14:16:53 +0000

    adicionando funções ao SilverPipeline

--- common/SilverPipelineClass.py
+++ common/SilverPipelineClass.py
@@ -23,15 +23,27 @@ class SilverPipeline(BasePipeline):
         print("Carga Silver finalizada com sucesso!")
 
     def run(self, source_table):
+        from pyspark.sql.function import current_timestamp
         print(f"Iniciando pipeline Silver para o domínio '{self.dominio}'...")
         
         # Extração dos dados da camada Bronze
         df_bronze = self.extract_from_bronze(source_table)
         
         # Transformações específicas da camada Silver podem ser aplicadas aqui
-        df_silver = df_bronze  # Placeholder para futuras transformações
+        df_silver = df_bronze.withColumn("ingest_timestamp", current_timestamp())
+        df_silver = self.drop_duplicates(df_silver) # Excluir duplicatas
+        df_silver = self.drop_nulls(df_silver) # Excluir nulos
         
         # Carga dos dados na camada Silver
         self.load_to_silver(df_silver, source_table)
         
         print(f"Pipeline Silver para o domínio '{self.dominio}' concluída com sucesso!")
+
+    def drop_duplicates(self, df: DataFrame) -> DataFrame:
+        df = df.dropDuplicates()
+        return df
+
+    def drop_nulls(self, df: DataFrame) -> DataFrame:
+        df = df.dropna()
+        return df
+
