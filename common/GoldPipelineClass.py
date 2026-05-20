2e1ea010b2c540663a4576a4b561cb36ce010470
Leandro Oliveira <leandro.oliveiravs@gmail.com>
2026-05-20 13:49:55 -0300

    GoldPipelineClass

--- common/GoldPipelineClass.py
+++ common/GoldPipelineClass.py
@@ -1,3 +1,22 @@
-class GoldPipeline:
-    # TODO: Implementar a pipeline Gold
-    pass
+from common.BasePipelineClass import BasePipeline
+from pyspark.sql import DataFrame
+
+class GoldPipeline(BasePipeline):
+    def __init__(self, dominio: str):
+        super().__init__(dominio, 'gold')
+
+    def extract_from_silver(self, table_name) -> 'pyspark.sql.DataFrame':
+        full_table_path = f"{self.catalog}.silver.{table_name}"
+        print(f"Extraindo dados de: {full_table_path}...")
+        
+        return self.spark.table(full_table_path)
+    
+    def load_to_gold(self, df, table_name):
+        full_table_path = f"{self.catalog}.{self.schema}.{table_name}"
+        
+        print(f"Salvando na Gold: {full_table_path}...")
+        
+        df.write.format("delta") \
+            .mode("overwrite") \
+            .option("overwriteSchema", "true") \
+            .saveAsTable(full_table_path)
