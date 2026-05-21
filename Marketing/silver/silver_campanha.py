e2f47808e1203e8a76e9f722508a34d03586e0c6
Leandro Oliveira <leandro.oliveiravs@gmail.com>
2026-05-21 17:30:11 -0300

    classe de campanha

---
+++ Marketing/silver/silver_campanha.py
@@ -0,0 +1,21 @@
+import pyspark
+from Marketing.silver.silver_base import MarketingSilverPipeline
+from pyspark.sql import DataFrame
+import pyspark.sql.functions as F
+
+class CampanhaMktPipeline(MarketingSilverPipeline):
+    def __init__(self):
+        super().__init__()
+
+    def transform(self, df:DataFrame) -> 'pyspark.sql.DataFrame':
+        print('Iniciando tratamento de campanhas...')
+        date_cols = ['data_inicio', 'data_fim']
+        decimal_cols = ['orcamento', 'gasto_real']
+        string_cols = ['nome', 'tipo', 'objetivo', 'status']
+
+        df = df \
+            .withColumn('nome', F.replace(F.col('nome'), ' -', '')) # Campanha 11 - Dia das Mães -> Campanha 11 Dia das Mães
+
+        df = super().transform(df, date_cols=date_cols, decimal_cols=decimal_cols, string_cols=string_cols)
+
+        return df
