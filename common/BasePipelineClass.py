f2d98544197e612b02a1df5092d0827b457238eb
Leandro Oliveira <leandro.oliveiravs@gmail.com>
2026-07-18 19:18:30 -0300

    resolvido erro do Delta localmente

--- common/BasePipelineClass.py
+++ common/BasePipelineClass.py
@@ -57,7 +57,9 @@ class BasePipeline:
                 .master("local[*]") \
                 .config("spark.driver.memory", "1g") \
                 .config("spark.executor.memory", "1g") \
-                .config("spark.jars.packages", "org.postgresql:postgresql:42.7.3") \
+                .config("spark.jars.packages", "org.postgresql:postgresql:42.7.3,io.delta:delta-spark_2.12:3.2.0") \
+                .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+                .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
                 .config("spark.sql.warehouse.dir", "/tmp/spark-warehouse") \
                 .config("spark.driver.extraJavaOptions", "-Dderby.stream.error.file=/tmp/derby.log") \
                 .getOrCreate()
